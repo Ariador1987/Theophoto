c7031f5b39bf5ec23f8419048a65c5ba883fff31
Božidar Bralić <bozidar.bralic@gmail.com>
2022-02-05 12:42:27 +0100

    Initial commit

---
+++ src/js/components/navbar.js
@@ -0,0 +1,5 @@
+// const toggleBtn = document.querySelector(".main-nav__hamburger");
+// const mainNavEl = document.querySelector(".main-nav");
+// toggleBtn.addEventListener("click", () => {
+//     mainNavEl.classList.toggle("show");
+// });
---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+import "./components/navbar.js";
+import TypeWriter from "./components/typewriter.js";
+
+const btnAboutEl = document.querySelector(".btn--cta");
+const aboutEl = document.querySelector(".about");
+
+function init() {
+    const txtElement = document.querySelector(".txt-type");
+    const words = JSON.parse(txtElement.getAttribute("data-words"));
+    const wait = txtElement.getAttribute("data-wait");
+    // Init TypeWriter
+    new TypeWriter(txtElement, words, wait);
+}
+
+btnAboutEl.addEventListener("click", () => {
+    aboutEl.scrollIntoView({ behavior: "smooth" });
+});
+
+document.addEventListener("DOMContentLoaded", init);
